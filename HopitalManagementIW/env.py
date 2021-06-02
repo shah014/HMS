7659ef671503c3669170663cb69e1cbe83f74aba
Anil Shah <45659928+shah014@users.noreply.github.com>
2021-06-02 16:05:23 +0545

    Update env.py

--- HopitalManagementIW/env.py
+++ HopitalManagementIW/env.py
@@ -1,8 +1,8 @@
 SECRET_KEY = 'l=68%$=0l362^ioel3ovqod(=k6c-@6em)r5@#9x91&8u__7@q'
 DEBUG = True
 
-
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = 'eb2787e5588a16'
-EMAIL_HOST_PASSWORD = 'e14106cd17c534'
-EMAIL_PORT = '2525'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'anilshah65156@gmail.com'
+EMAIL_HOST_PASSWORD = 'cvxadadszfaoikax'
