fb538bd2137c0c26f91efdf3c34192840ed4fb04
Anil Shah <45659928+shah014@users.noreply.github.com>
2021-06-02 12:21:10 +0545

    Update settings.py

--- HopitalManagementIW/settings.py
+++ HopitalManagementIW/settings.py
@@ -138,9 +138,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'anilshah65156@gmail.com'
-EMAIL_HOST_PASSWORD = 'shantishah01'
-EMAIL_PORT = 587
